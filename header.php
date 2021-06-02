89b7a31fd5ef5558b0909cf7baf6c69e68054427
NatsumiMATSUDA <p17082mn@gm.tsuda.ac.jp>
2021-06-02 23:56:28 +0900

    contact page doesn't read css.

--- header.php
+++ header.php
@@ -1,16 +1,32 @@
+<!DOCTYPE html>
+<html>
 
-<header>
-  <h1>
-    <a href="../top.php">漢口商会</a>
-  </h1>
-  <nav>
-    <ul>
-      <li><a href="#">会社概要</a></li>
-      <li><a href="#">修理依頼</a></li>
-      <li><a href="#">出張範囲</a></li>
-      <li><a href="#">製品販売</a></li>
-      <li><a href="#">アクセス</a></li>
-      <li><a href="contact/contact.php">お問い合わせ</a></li>
-    </ul>
-  </nav>
-</header>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css_folder/header.css">
+    <title>漢口商会 お問い合わせ</title>
+  </head>
+
+  <body>
+
+    <header>
+      <h1>
+        <a href="../top.php">漢口商会</a>
+      </h1>
+      <nav>
+        <ul>
+          <li><a href="#">会社概要</a></li>
+          <li><a href="#">修理依頼</a></li>
+          <li><a href="#">出張範囲</a></li>
+          <li><a href="#">製品販売</a></li>
+          <li><a href="#">アクセス</a></li>
+          <li><a href="contact/contact.php">お問い合わせ</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  </body>
+</html>
