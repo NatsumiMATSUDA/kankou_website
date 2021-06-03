91b048d9218c41fc4b72e7ee726f09402d081950
NatsumiMATSUDA <p17082mn@gm.tsuda.ac.jp>
2021-06-03 23:26:45 +0900

    Cleaned up some files and connect php and css

--- contact/contact.php
+++ contact/contact.php
@@ -113,7 +113,7 @@ if (isset($_POST['submitted'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../stylesheet.css">
     <title>漢口商会 お問い合わせ</title>
   </head>
 
--- footer.php
+++ footer.php
@@ -1,28 +1,9 @@
-<!DOCTYPE html>
-<html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css_folder/footer.css">
-    <title>漢口商会 お問い合わせ</title>
-  </head>
-
-  <body>
-
-    <footer>
-      <h3>有限会社 漢口商会</h3>
-      <p>埼玉県さいたま市浦和区仲町1-9-13カンコウビル1階</br>
-        電話番号: 048-822-4890
-      </p>
-      <div class="">
-
-      </div>
-
-    </footer>
-
-</body>
-
-</html>
+<footer>
+  <h3>有限会社 漢口商会</h3>
+  <p>埼玉県さいたま市浦和区仲町1-9-13カンコウビル1階</br>
+    電話番号: 048-822-4890
+  </p>
+  <div class="">
+  </div>
+</footer>
